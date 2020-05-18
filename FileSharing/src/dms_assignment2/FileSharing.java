55973c46385a7e5e9f8eba08107f10206f3e4379
Jesselle <dqp6065@autuni.ac.nz>
2020-05-18 14:17:26 +1200

    set up

---
+++ FileSharing/src/dms_assignment2/FileSharing.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dms_assignment2;
+
+/**
+ *
+ * @author dqp6065 and ydw6617
+ */
+public class FileSharing {
+    
+}
---
+++ FileSharing/src/dms_assignment2/Peer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dms_assignment2;
+
+/**
+ *
+ * @author dqp6065 and ydw6617
+ */
+public class Peer {
+    private int peerId;
+    
+    //THIS ASSUMES ONLY ONE FILE TO SEND AND RECIEVE AT A TIME
+    //file to send
+    //file to recieve
+    
+    private int port;
+}
